81da65a1aff77c771908cc13d9be0b10f54e2904
Aldo Estela <aldo.david@gmail.com>
2024-03-16 23:36:14 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import { Link, Route, Switch } from 'react-router-dom/cjs/react-router-dom.min';
+import Home from './components/Home';
+import CreateAccount from './components/CreateAccount';
+import Deposit from './components/Deposit';
+import Withdraw from './components/Withdraw';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Aldo's Bad Bank v1.0</h1>
       </header>
+      <nav>
+        <ul className="nav justify-content-center nav-pills">
+          <li className="nav-item">
+            <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+          </li>
+          <li class="nav-item">
+            <Link class="nav-link" to="/createAccount">Create Account</Link>
+          </li>
+          <li class="nav-item">
+            <Link class="nav-link "  to="/deposit">Deposit</Link>
+          </li>
+          <li class="nav-item">
+            <Link class="nav-link "  to="/withdraw">Withdraw</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <hr />
+      <div className="page-div-load">
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/createAccount" component={CreateAccount} />
+          <Route exact path="/deposit" component={Deposit} />
+          <Route exact path="/withdraw" component={Withdraw} />
+        </Switch>
+      </div>
+      
     </div>
   );
 }
